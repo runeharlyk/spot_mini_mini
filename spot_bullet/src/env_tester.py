ab3b6702751dbc3b2c5329d390744195455abad0
Rune Harlyk <rune@harlyk.dk>
2024-03-01 15:09:06 +0100

    Removes unused code

--- spot_bullet/src/env_tester.py
+++ spot_bullet/src/env_tester.py
@@ -12,34 +12,6 @@ from spotmicro.Kinematics.SpotKinematics import SpotModel
 from spotmicro.GaitGenerator.Bezier import BezierGait
 from spotmicro.OpenLoopSM.SpotOL import BezierStepper
 
-STANCE = 0
-SWING = 1
-
-
-def TransToRp(T):
-    """
-    Converts a homogeneous transformation matrix into a rotation matrix
-    and position vector
-
-    :param T: A homogeneous transformation matrix
-    :return R: The corresponding rotation matrix,
-    :return p: The corresponding position vector.
-
-    Example Input:
-        T = np.array([[1, 0,  0, 0],
-                      [0, 0, -1, 0],
-                      [0, 1,  0, 3],
-                      [0, 0,  0, 1]])
-
-    Output:
-        (np.array([[1, 0,  0],
-                   [0, 0, -1],
-                   [0, 1,  0]]),
-         np.array([0, 0, 3]))
-    """
-    T = np.array(T)
-    return T[0:3, 0:3], T[0:3, 3]
-
 
 class GaitState:
     def __init__(self) -> None:
@@ -64,79 +36,6 @@ class BodyState:
         self.position = np.array([0, 0, 0])
         self.rotation = np.array([0, 0, 0])
         self.worldFeetPositions = {}
-
-
-# class NewBezierGait:
-#     def __init__(self) -> None:
-#         self.dSref = [0.0, 0.0, 0.5, 0.5]
-#         self.dSref = dSref
-#         self.Prev_fxyz = [0.0, 0.0, 0.0, 0.0]
-#         self.NumControlPoints = 11
-
-#         self.time = 0.0
-#         # Touchdown Time
-#         self.touchDownTime = 0.0
-#         self.time_since_last_TD = 0.0
-#         self.StanceSwing = SWING
-#         self.SwRef = 0.0
-#         self.Stref = 0.0
-#         # Whether Reference Foot has Touched Down
-#         self.TD = False
-
-#         # Stance Time
-#         self.Tswing = 0.2
-
-#         self.ref_idx = 0
-
-#         # Store all leg phases
-#         self.Phases = self.dSref
-
-#     def generate_trajectory(self, body_state: BodyState, gait_state: GaitState, dt):
-#         if gait_state.stepVelocity != 0:
-#             tStance = 2.0 * abs(gait_state.stepLength) / abs(gait_state.stepVelocity)
-#         else:
-#             tStance = 0.0
-#             gait_state.stepLength = 0.0
-#             self.TD = False
-#             self.time = 0.0
-#             self.time_since_last_TD = 0.0
-
-#         gait_state.yawRate *= dt
-
-#         if tStance < dt or tStance > 1.3 * gait_state.swingPeriod:
-#             tStance = max(0.0, min(tStance, 1.3 * gait_state.swingPeriod))
-#             gait_state.stepLength, gait_state.yawRate = 0.0, 0.0
-#             self.TD = False
-#             self.time = 0.0
-#             self.time_since_last_TD = 0.0
-
-#         if gait_state.contacts[0] and tStance > dt:
-#             self.TD = True
-
-#         self.increment(dt, tStance + gait_state.swingPeriod)
-
-#         T_bf = copy.deepcopy(body_state.worldFeetPositions)
-#         ref_dS = {"FL": 0.0, "FR": 0.5, "BL": 0.5, "BR": 0.0}
-#         for i, (key, Tbf_in) in enumerate(body_state.worldFeetPositions.items()):
-#             self.ref_idx = i if key == "FL" else self.ref_idx
-#             self.dSref[i] = ref_dS[key]
-#             _, p_bf = TransToRp(Tbf_in)
-#             step_coord = (
-#                 self.get_foot_step(body_state, gait_state, i, key)
-#                 if tStance > 0
-#                 else np.array([0.0, 0.0, 0.0])
-#             )
-#             for j in range(3):
-#                 T_bf[key][j, 3] += step_coord[j]
-#         return T_bf
-
-#     def get_foot_step(self, body_state: BodyState, gait_state: GaitState):
-#         phase, stance_swing = self.get_phase(gait_state)
-#         self.phases[gait_state.index] = phase + 1.0 if stance_swing == SWING else phase
-
-#         if stance_swing == STANCE:
-#             return self.stance_step(phase, gait_state, body_state)
-#         return self.swing_step(phase, gait_state, body_state)
 
 
 class Gait:
